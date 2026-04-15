2a89ccbec23bab197aebae37a2a964cd5575032c
sayanic601-debug <sayanic601@gmail.com>
2026-04-15 12:04:07 +0530

    Added alphanumeric ID generator

---
+++ src/utils/idGenerator.js
@@ -0,0 +1,15 @@
+// utils/randomAlphaNumericGenerator.js
+
+function generateRandomAlphaNumeric(length = 10) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        result += chars[randomIndex];
+    }
+
+    return result;
+}
+
+module.exports = generateRandomAlphaNumeric;
