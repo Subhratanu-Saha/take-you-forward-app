cfd13a3c34f443b0700209fa3f2b65db0c2c1d4e
Suvam-paul145 <suvampersonal555@gmail.com>
2026-04-22 07:35:07 +0530

    FEAT: HTTP routes are created for subscriber

---
+++ src/routes/subscriiberRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const subscriberController = require('../controllers/subscriberController');
+const { validateCreateSubscriber } = require('../middleware/subscriberValidator');
+
+// GET subscriber by customer ID [/api/v1/subscriber?customerid=X]
+router.get('/', subscriberController.getSubscriberByCustomerId);
+
+// CREATE new subscriber [/api/v1/subscriber]
+router.post('/', validateCreateSubscriber, subscriberController.createSubscriberRecord);
+
+// UPDATE subscriber [/api/v1/subscriber]
+router.put('/', subscriberController.updateSubscriberRecord);
+
+module.exports = router;
