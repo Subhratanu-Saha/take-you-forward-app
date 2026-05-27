83895d1856062ef2e81276100ebe3f3573156d9b
Souvik Sarkar <sarkarsouvik107@gmail.com>
2026-05-27 23:03:01 +0530

    added promotionalMessageController File

--- src/controllers/promotionalMessageController.js
+++ src/controllers/promotionalMessageController.js
@@ -1,9 +1,22 @@
 // controllers/promotionalMessageController.js
+
 const promotionalMessageService = require('../services/promotionalMessageService');
 
 const createPromotionalMessage = async (req, res) => {
+
   try {
+
     const data = req.body;
+
+    // 400 Validation Error
+    if (!data.title || !data.message) {
+
+      return res.status(400).json({
+        success: false,
+        message: 'Title and message are required'
+      });
+
+    }
 
     const result =
       await promotionalMessageService.createPromotionalMessage(data);
@@ -22,7 +35,9 @@ const createPromotionalMessage = async (req, res) => {
     });
 
   }
+
 };
+
 module.exports = {
   createPromotionalMessage
 };
